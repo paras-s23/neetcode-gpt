1305a808d571bc7050a5f249dc0d9050961e1e88
Paras Sharma <paras.usa2006@gmail.com>
2026-06-18 15:14:42 -0400

    Implement MLP from scratch (Build a Neural Net 4/5)

---
+++ foundations/mlp.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import List
+
+
+class Solution:
+    def forward(self, x: NDArray[np.float64], weights: List[NDArray[np.float64]], biases: List[NDArray[np.float64]]) -> NDArray[np.float64]:
+        # x: 1D input array
+        # weights: list of 2D weight matrices
+        # biases: list of 1D bias vectors
+        # Apply ReLU after each hidden layer, no activation on output layer
+        # return np.round(your_answer, 5)
+        output = x
+        
+
+        for i in range(len(weights)):
+            output = output @ weights[i]
+            output += biases[i]
+            output = np.maximum(0,output)
+            if i == (len(weights)-1):
+                break
+        return output 
+            
+
